4f6e7bb28500f99bec693456b20a24e4362cd8ed
National <nationalshrestha10@gmail.com>
2023-06-19 20:47:38 +0545

    practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+// import "./App.css";
+import BasicTable from "./component/BasicTable";
+
+function App() {
+  return (
+    <>
+      <BasicTable />
+    </>
+  );
+}
+
+export default App;
